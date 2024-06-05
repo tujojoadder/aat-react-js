38e6e0a36e061bff569b8678e267e8bf733f9bc3
turjojoadder <turjojoadder10@gmail.com>
2024-06-06 04:02:19 +0600

    hadith Moment done

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,22 +1,139 @@
 import React from "react";
 import "./Home.css";
-
+import image from "./logo.jpg";
 export default function Home() {
   return (
     <div className="">
       <div className="border-right">
         <div className="wrapper">
-          <div className="item">boh</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
-          <div className="item">box1</div>
+          <div className="item mt-1 pb-2">
+            <div className="user-div ">
+              <div class="d-flex flex-column bd-highlight ">
+                <div class="mt-2 mb-1 bd-highlight">
+                  <img
+                    src={image}
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      borderRadius: "100%",
+                      border: "2px solid blue",
+                    }}
+                    alt=""
+                  />
+                </div>
+                <div class=" px-1  bd-highlight name">
+                  <p>
+                    <b>Inamul </b>
+                  </p>
+                </div>
+                <div class=" bd-highlight px-1">
+                  এ মর্মে আল্লাহ্ তা’আলার 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item mt-1 ">
+            <div className="user-div ">
+              <div class="d-flex flex-column bd-highlight ">
+                <div class="mt-2 mb-1 bd-highlight">
+                  <img
+                    src={image}
+                    style={{
+                      width: "45px",
+                      height: "45px",
+                      borderRadius: "100%",
+                      border: "2px solid blue",
+                    }}
+                    alt=""
+                  />
+                </div>
+                <div class=" px-1 bd-highlight name">
+                  <p>
+                    <b>Inamul huqe chokroboty</b>
+                  </p>
+                </div>
+                <div class=" bd-highlight px-1">
+                  এ মর্মে আল্লাহ্ তা’আলার
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item mt-1 ">
+            <div className="user-div ">
+              <div class="d-flex flex-column bd-highlight ">
+                <div class="mt-2 mb-1 bd-highlight">
+                  <img
+                    src={image}
+                    style={{
+                      width: "45px",
+                      height: "45px",
+                      borderRadius: "100%",
+                      border: "2px solid blue",
+                    }}
+                    alt=""
+                  />
+                </div>
+                <div class=" px-1 bd-highlight name">
+                  <p>
+                    <b>Inamul huqe chokroboty</b>
+                  </p>
+                </div>
+                <div class=" bd-highlight px-1">
+                  এ মর্মে আল্লাহ্ তা’আলার
+                </div>
+              </div>
+            </div>
+          </div><div className="item mt-1 ">
+            <div className="user-div ">
+              <div class="d-flex flex-column bd-highlight ">
+                <div class="mt-2 mb-1 bd-highlight">
+                  <img
+                    src={image}
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                      borderRadius: "100%",
+                      border: "2px solid blue",
+                    }}
+                    alt=""
+                  />
+                </div>
+                <div class=" px-1 bd-highlight name">
+                  <p>
+                    <b>Inamul huqe chokroboty</b>
+                  </p>
+                </div>
+                <div class=" bd-highlight px-1">
+                  এ মর্মে আল্লাহ্ তা’আলার
+                </div>
+              </div>
+            </div>
+          </div><div className="item mt-1 ">
+            <div className="user-div ">
+              <div class="d-flex flex-column bd-highlight ">
+                <div class="mt-2 mb-1 bd-highlight">
+                  <img
+                    src={image}
+                    style={{
+                      width: "9vh",
+                      height: "9vh",
+                      borderRadius: "100%",
+                      border: "2px solid blue",
+                    }}
+                    alt=""
+                  />
+                </div>
+                <div class=" px-1 bd-highlight name">
+                  <p>
+                    <b>Inamul huqe chokroboty</b>
+                  </p>
+                </div>
+                <div class=" bd-highlight px-1">
+                  এ মর্মে আল্লাহ্ তা’আলার
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
