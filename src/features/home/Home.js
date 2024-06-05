e20ab735585ca833d338abdd51b56f76be5f46a2
turjojoadder <turjojoadder10@gmail.com>
2024-06-06 02:16:45 +0600

    hadith Moment layout done final--

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,26 +1,24 @@
-import React, { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import './Home.css';
+import React from "react";
+import "./Home.css";
+
 export default function Home() {
-
-
   return (
-  <div>
-    <div className="border-right">
-    <div className="wrapper">
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
-      <div className="item">box1</div>
+    <div className="">
+      <div className="border-right">
+        <div className="wrapper">
+          <div className="item">boh</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+          <div className="item">box1</div>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
   );
 }
