48c21c4fcccfe64fed8beb18894c28ac28d32dce
turjojoadder <turjojoadder10@gmail.com>
2024-06-16 04:22:44 +0600

    home and home left all component done

--- src/features/home/Components/HadithDay/HadithDay.js
+++ src/features/home/Components/HadithDay/HadithDay.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React from 'react'
+import image from "./logo.jpg";
+import image1 from "./logo1.png";
+import image2 from "./logo2.png";
 import HadithBox from '../Hadithbox/HadithBox';
+import HadithDayContent from './HadithDayContent';
+import { useMediaQuery } from "react-responsive";
 
 export default function HadithDay() {
+    const isNotSm = useMediaQuery({ minWidth: 576 }); // Bootstrap's sm breakpoint is 576px
+
   return (
-    <div className="container">
-      <div id="carouselExample" className="carousel slide row">
-        
-        <div className="col-3 d-flex align-items-center justify-content-center">
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <div className={isNotSm ? "container-sm" : ""} style={{overflow:"hidden"}}>
+      <div className="row " >
+        {/* Previous button taking first 3 columns */}
+        <div className="col-3 d-none d-xl-block   d-flex align-items-center justify-content-center">
+          <button className=" carousel-dark carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </button>
         </div>
-        
-        <div className="col-6">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <HadithBox />
+
+        {/* Carousel taking middle 6 columns */}
+        <div className="col-xl-6 ">
+          <div id="carouselExample" className="carousel slide">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <HadithDayContent/>
+              </div>
+              <div className="carousel-item">
+                <img src={image2} className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+              <HadithDayContent/>
+              </div>
             </div>
-            <div className="carousel-item">
-              <HadithBox />
-            </div>
-            <div className="carousel-item">
-              <HadithBox />
-            </div>
+
+            <button className="carousel-dark carousel-control-prev   d-xl-none" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span className="visually-hidden">Previous</span>
+</button>
+<button className="carousel-dark carousel-control-next  d-xl-none" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+  <span className="visually-hidden">Next</span>
+</button>
+
+
+
+
+            
           </div>
         </div>
-        
-        <div className="col-3 d-flex align-items-center justify-content-center">
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+
+        {/* Next button taking last 3 columns */}
+        <div className="col-3  d-none d-xl-block  d-flex align-items-center justify-content-center">
+          <button className=" carousel-dark carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
           </button>
         </div>
       </div>
     </div>
-  );
+  )
 }
---
+++ src/features/home/Components/HadithDay/HadithDayContent.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function HadithDayContent() {
+  return (
+    <div class="mb-1 ms-2 haddis ">
+    <div
+      class="hadis-head  bg-info d-flex justify-content-between"
+      style={{ height: "10vh" ,marginTop:'1vh' }}
+    >
+      {/* Copy button */}
+      <button
+        style={{ borderRadius: "30px" }}
+        class="btn btn-info btn-sm btn-custom ml-3 my-2"
+      >
+        Copy
+        <i class="fa-solid fa-copy"></i>
+      </button>
+      {/* New hadis button */}
+      <div class="btn btn-info btn-sm mr-3 my-2 rounded-circle d-flex align-items-center justify-content-center">
+        <i class="fa-solid fa-rotate text-light"></i>
+      </div>
+    </div>
+
+    <div
+      className=" card-body custom-scroll bg-opacity-10 bg-black"
+      style={{
+        height: "88vh",
+        borderBottomRightRadius: "30px",
+        borderBottomLeftRadius: "30px",
+      }}
+    >
+      <div className="">
+        <h5 className="card-title">Primary card title</h5>
+        <p className="card-text">
+          এ মর্মে আল্লাহ্ তা’আলার বাণীঃ ’’নিশ্চয় আমি আপনার প্রতি সেরূপ
+          ওয়াহী প্রেরণ করেছি যেরূপ নূহ ও তাঁর পরবর্তী নবীদের (নবীদের)
+          প্রতি ওয়াহী প্রেরণ করেছিলাম।’’ (সূরাহ্ আন-নিসা ৪/১৬৩) ১.
+          ’আলক্বামাহ ইবনু ওয়াক্কাস আল-লায়সী (রহ.) হতে বর্ণিত। আমি
+          ’উমার ইবনুল খাত্তাব (রাঃ)-কে মিম্বারের উপর দাঁড়িয়ে বলতে
+          শুনেছিঃ আমি আল্লাহর রাসূল সাল্লাল্লাহু আলাইহি ওয়াসাল্লাম-কে
+          বলতে শুনেছিঃ কাজ (এর প্রাপ্য হবে) নিয়্যাত অনুযায়ী। আর মানুষ
+          তার নিয়্যাত অনুযায়ী প্রতিফল পাবে। তাই যার হিজরত হবে ইহকাল
+          লাভের অথবা কোন মহিলাকে বিবাহ করার উদ্দেশে- তবে তার হিজরত সে
+          উদ্দেশেই হবে, যে জন্যে, সে হিজরত করেছে।] (৫৪, ২৫২৯, ৩৮৯৮,
+          ৫০৭০, ৬৬৮৯, ৬৯৫৩; মুসলিম ২৩/৪৫ হাঃ ১৯০৭, আহমাদ ১৬৮) ( আধুনিক
+          প্রকাশনী- ১, ইসলামিক ফাউন্ডেশন ১)
+        </p>
+      </div>
+    </div>
+  </div>
+  )
+}
