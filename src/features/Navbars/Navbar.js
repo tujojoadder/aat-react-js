77a6c2791cfa971f7bb6f93ecf16b670478db65e
turjojoadder <turjojoadder10@gmail.com>
2024-06-23 00:36:51 +0600

    home done

--- src/features/Navbars/Navbar.js
+++ src/features/Navbars/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = () => {
   const isNotSm = useMediaQuery({ minWidth: 576 }); // Bootstrap's sm breakpoint is 576px
 
   return (
-    <Scrollbars style={{ width: '100%', height:'105vh' }} >
-  <div className={isNotSm ? "container-sm " : "ms-3 me-3"} 
+    <Scrollbars style={{ width: '100%', height:'104vh',minHeight:'300px' }} >
+  <div className={isNotSm ? "container-sm  " : "ms-3 me-3"} 
     style={{
         backgroundColor: '#ffffff',  
         background: 'linear-gradient(to right, #ffffff 20%, #f0f2f5 80%)'
@@ -50,9 +50,9 @@ const Navbar = () => {
 
       <div className="row">
         {/* LEFT SIDE BAR */}
-        <div style={{backgroundColor:'#f0f2f5',height:'103vh'}} className="p-0  d-none col-sm-2 d-sm-block col-md-2 col-lg-3 d-flex flex-column text-sm-end text-md-start align-items-lg-start left_sidebar  ">
+        <div style={{backgroundColor:'#f0f2f5',height:'100vh',}} className="p-0  d-none col-sm-2 d-sm-block col-md-2 col-lg-3 d-flex flex-column text-sm-end text-md-start align-items-lg-start left_sidebar  ">
 
-          <div className="pe-2 pt-5   bg-body rounded" style={{width:'88%',height:'105vh'}}>
+          <div className="pe-2 pt-5   bg-body rounded" style={{width:'88%',height:'100vh'}}>
           <div className="  mb-3 d-flex align-items-center justify-content-center mydiv">
             <span>
               <i className="fa-brands fa-twitter display-5 text-info"></i>
@@ -140,6 +140,9 @@ const Navbar = () => {
           </Routes>
         </div>
       </div>
+
+
+      
     </div>
     </Scrollbars>
   );
--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -8,10 +8,11 @@ import TextPost from "./Components/TextPost/TextPost";
 import BothPost from "./Components/BothPost/BothPost";
 import ImagePost from "./Components/ImagePost/ImagePost";
 import HadithStatus from "./Components/HadithStatus/HadithStatus";
-
+import { Scrollbars } from 'react-custom-scrollbars';
 export default function Home() {
   return (
-    <div className="p-0 m-0 mt-3 border" style={{ width: "100%" }}>
+    
+    <div className="p-0 m-0  border" style={{ width: "100%" }}>
 
    {/*    Hadith Status */}
       <HadithStatus />
