4dd240e4e95754ad075a40846aea6269d99b9663
turjojoadder <turjojoadder10@gmail.com>
2024-08-23 19:55:17 +0600

    profile about done from without change profile

--- src/features/LargeScreenBack/LargeScreenProfileBack.js
+++ src/features/LargeScreenBack/LargeScreenProfileBack.js
@@ -5,11 +5,11 @@ export default function LargeScreenProfileBack({text}) {
     const navigate = useNavigate();
 
   return (
-    <div className="d-none  d-md-flex align-items-center justify-content-between pb-1 ms-2 ">
+    <div className="d-none pt-1 d-md-flex align-items-center justify-content-between pb-1 ms-2 ">
     <button className="btn btn-link pt-2 p-0 m-0"  onClick={() => navigate(-1)} >
       <i className="fas fa-arrow-left fs-4"></i>
     </button>
-    <h5 className="flex-grow-1 ps-3 pt-1  text-left mb-0">{text}</h5>
+    <h5 style={{fontWeight:'bolder'}} className="flex-grow-1 ps-3 pt-1  text-left mb-0">{text}</h5>
   </div>
   )
 }
